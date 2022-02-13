4b77c63f63f9a3e993e9eb1df163fb71534205e1
zonief <yosachot@gmail.com>
2022-02-13 16:13:55 +0100

    Initial commit

---
+++ src/main/java/com/example/cloudeventsdemo/CloudEventsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudeventsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudEventsDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEventsDemoApplication.class, args);
+    }
+
+}
